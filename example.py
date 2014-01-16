bf9dfc3caa506eea745906fa667555d08e99b029
Alexandr Kalenuk <akalenuk@gmail.com>
2014-01-16 16:47:40 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,15 +1,15 @@
 from fakelisp import *
 
-(BEGIN
+# And now you can write lisp
+X = (BEGIN
 	(SET (F) (LAMBDA (X)
 		(IF (EQ (X) (1))
 			(1)
 			(MUL (X) (F (SUB (X) (1)))))))
 
-	(SET (X) (QUOTE (F (4)) (42))))
+	(QUOTE (F (4)) (42)))
 
-print X
+# Back to Python any time
+print "x: ", X
 
-# Yes, it does print [24, 42].
-#
-# Isn't it awesome?
+# It does print [24, 42], but better check it yourself
