f675b121e2a186494fea7dea047b3c0f0b2b5853
Oleksandr Kaleniuk <akalenuk@gmail.com>
2022-12-29 18:47:39 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,15 +1,15 @@
 from fakelisp import *
 
-# And now you can write lisp
+# And now you can start mixing Python and LISP
 X = (BEGIN
-	(SET (F) (LAMBDA (X)
-		(IF (EQ (X) (1))
-			(1)
-			(MUL (X) (F (SUB (X) (1)))))))
+        (SET (F) (LAMBDA (X)
+            (IF (EQ (X) (1))
+                (1)
+                (MUL (X) (F (SUB (X) (1)))))))
 
-	(QUOTE (F (4)) (42)))
+        (LIST (F (4)) (42)))
 
 # Back to Python any time
-print "x: ", X
+print "x: ", str(X)
 
-# It does print [24, 42], but better check it yourself
+# It prints [24, 42]
